a832f59adf0a9440b52b7d0773c11d4018a2ef1b
Nick Koukis <nikoskoukis99@gmail.com>
2023-10-14 00:47:12 +0300

    test json data

--- src/views/Home/components/NewUserForm/ViewWallet.tsx
+++ src/views/Home/components/NewUserForm/ViewWallet.tsx
@@ -8,9 +8,13 @@ interface UserInfo {
 interface ViewWalletProps {
   userInfo: UserInfo;
   isLoadingUserInfo: boolean;
+  jsonPrettyData: string;
 }
 
-const ViewWallet: React.FC<ViewWalletProps> = ({ userInfo, isLoadingUserInfo }) => {
+const ViewWallet: React.FC<ViewWalletProps> = ({ userInfo, isLoadingUserInfo, jsonPrettyData }) => {
+
+  const parsedData = JSON.parse(jsonPrettyData);
+  
   return (
     <div>
       {isLoadingUserInfo ? (
@@ -20,6 +24,13 @@ const ViewWallet: React.FC<ViewWalletProps> = ({ userInfo, isLoadingUserInfo }) 
           <h1>View Wallet</h1>
           <p>Address: {userInfo.address}</p>
           <p>Secret Words: {userInfo.secretWords.join(', ')}</p>
+          <div>
+            {Object.keys(parsedData).map(key => (
+              <div key={key}>
+                 <strong>{key}:</strong> {parsedData[key]}
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
