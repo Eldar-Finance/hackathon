1bb4ab6287704462ac43516f953c85c4c2954837
klepi21 <kolepidas@gmail.com>
2023-10-13 22:45:32 +0300

    exist

--- src/views/Home/components/ExistingUser/ExistingUser.tsx
+++ src/views/Home/components/ExistingUser/ExistingUser.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react';
 import {
-  Button,
   ChakraProvider,
   Container,
   FormControl,
   Input,
   Stack,
-  Text,
-  VStack,
   extendTheme,
   CSSReset,
-  Box,
   Grid,
   Modal,
   ModalOverlay,
@@ -20,14 +16,17 @@ import {
   ModalFooter,
   ModalCloseButton,
   Alert,
-  AlertIcon,
+  AlertIcon,Flex,
 } from '@chakra-ui/react';
 import { IScUserInfo } from "@/utils/types/sc.interface";
 import { deleteWallet } from '../../services/calls';
 import { createEncryptionKey, decrypt } from '@/utils/functions/cryptography';
+import { Box, Button, Text, VStack, HStack, Circle, Icon, List, ListItem } from "@chakra-ui/react";
+import { FaBeer, FaCoffee, FaPhabricator,FaEraser } from 'react-icons/fa';
+
 
 const theme = extendTheme({
-  initialColorMode: 'light',
+  initialColorMode: 'dark',
 });
 
 function ExistingUser({address, email, secretWords, userGid}: {address: string, email: string, secretWords: string[], userGid: string}) {
@@ -65,20 +64,85 @@ function ExistingUser({address, email, secretWords, userGid}: {address: string, 
     };
 
     return (
-    <ChakraProvider theme={theme}>
-        <CSSReset />
-        <Container maxW={'800px'}>
-            <Text>
-                <b>Address:</b> {address}
+        <Container maxW={'800px'} backgroundColor={'#121212'}>
+        <VStack spacing={6} p={4}>
+
+{/* Header */}
+<HStack width="100%" justifyContent="space-between">
+  
+<Text>
+                <b> Wallet Address:</b> {address}
             </Text>
-            <Button
-                m={5}
-                onClick={handleClickWords}
-                size="sm"
-                colorScheme="teal"
-            >
-                Words
-            </Button>
+  <Button variant="link">Exit wallet</Button>
+</HStack>
+
+<Box bg="gray.800" p={6} borderRadius="md" width="100%">
+      <Flex justifyContent="space-between" alignItems="center">
+        <VStack alignItems="start" spacing={2}>
+          <Text fontSize="lg" color="gray.400">Overview</Text>
+          <Text fontSize="2xl" color="white">134.23 EGLD</Text>
+          <HStack justifyContent="space-between">
+            <Text fontSize="md" color="gray.400">$835.39 USD</Text>
+            {/* Add other details like percentage change here */}
+          </HStack>
+        </VStack>
+
+        <HStack spacing={4}>
+          <Button
+            bg="transparent"
+            p={0}
+            _hover={{ bg: "transparent" }}
+            _active={{ bg: "transparent" }}
+            _focus={{ boxShadow: "none" }}
+            onClick={handleClickWords}
+          >
+            <VStack spacing={1}>    
+              <Circle size="50px" bg="blue.500" color="white">
+                <Icon as={FaPhabricator} w="24px" h="24px" />
+              </Circle>
+              <Text fontSize="sm">Seed Phrase</Text>
+            </VStack>
+          </Button>
+          <Button
+            bg="transparent"
+            p={0}
+            _hover={{ bg: "transparent" }}
+            _active={{ bg: "transparent" }}
+            _focus={{ boxShadow: "none" }}
+            onClick={() => handleDeleteWallet(email)}
+          >
+            <VStack spacing={1}>    
+              <Circle size="50px" bg="blue.500" color="white">
+                <Icon as={FaEraser} w="24px" h="24px" />
+              </Circle>
+              <Text fontSize="sm">Delete Wallet</Text>
+            </VStack>
+          </Button>
+
+        </HStack>
+      </Flex>
+    </Box>
+
+{/* Light Component */}
+<Box bg="gray.100" p={6} borderRadius="md" width="100%">
+  <Text fontSize="lg" color="gray.800">Recent transactions</Text>
+  <List spacing={4} mt={4}>
+  <ListItem>
+    <Box display="flex" alignItems="center">
+        <VStack align="start">
+            <Text fontSize="sm" color="gray.600">Received from erd1u66v59...</Text>
+            <Text fontSize="sm" color="gray.600">March 27th 2020, 3:39 PM</Text>
+        </VStack>
+        <Text fontSize="md" color="green.500" ml="auto">+100.20 ERD</Text>
+    </Box>
+</ListItem>
+
+  </List>
+</Box>
+
+</VStack>
+            
+            
             {isWordsVisible && (
                 <Grid templateColumns="repeat(6, 1fr)" gap={6}>
                 {secretWords.map((word, index) => (
@@ -124,16 +188,8 @@ function ExistingUser({address, email, secretWords, userGid}: {address: string, 
                     Incorrect PIN. Please try again.
                 </Alert>
             )}
-            <Button
-                m={5}
-                onClick={() => handleDeleteWallet(email)}
-                size="sm"
-                colorScheme="teal"
-            >
-                Delete Wallet
-            </Button>
+
         </Container>
-    </ChakraProvider>
     );
 }
 
