3bdc01d84bddd85bcb27547c7ccd7d28ff7bbda5
Nick Koukis <nikoskoukis99@gmail.com>
2023-10-18 22:30:20 +0300

    test env vars

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,11 +5,11 @@ import { Session, DefaultSession } from "next-auth"
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '914628494898-onc9q6mppitoa680q721r42ki64hh556.apps.googleusercontent.com',
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || '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',
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXT_PUBLIC_AUTH_SECRET || 'hackathon',
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET || "",
   callbacks: {
     session({ session, token }) {
       session.user.sub  = token.sub ;
